1ea0d6b9b536c1b3a7124f28b688e9bb65755223
huntertigert <huntermtigert@gmail.com>
2025-03-20 13:30:44 -0500

    Commit: left and right movement

--- main.py
+++ main.py
@@ -3,13 +3,20 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
 
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -20,8 +27,12 @@ def main():
                 return
             
         screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 
